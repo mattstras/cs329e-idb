945411714ec88843717ccf07e51b49a40fc599a8
Matthew Larsen <mel2842@cs.utexas.edu>
2017-04-17 16:19:06 -0500

    added routing functions for all pages

--- app.py
+++ app.py
@@ -31,6 +31,102 @@ def state():
 @app.route('/positions')
 def positions():
 	return render_template('positions.html') 
+
+@app.route('/alanmorales')
+def alanmorales():
+	return render_template('alanmorales.html')
+	
+@app.route('/alaska')
+def alaska():
+	return render_template('alaska.html') 
+
+@app.route('/california')
+def california():
+	return render_template('california.html') 
+
+@app.route('/chiefjustice')
+def chiefjustice():
+	return render_template('chiefjustice.html') 
+
+@app.route('/chienchihhuang')
+def chienchihhuang():
+	return render_template('chienchihhuang.html')
+	
+@app.route('/donaldtrump')
+def donaldtrump():
+	return render_template('donaldtrump.html') 
+
+@app.route('/governor')
+def governor():
+	return render_template('governor.html') 
+
+@app.route('/gregabbott')
+def gregabbott():
+	return render_template('gregabbott.html') 
+
+@app.route('/johnrobert')
+def johnrobert():
+	return render_template('johnrobert.html')
+	
+@app.route('/lisamurkowski')
+def lisamurkowski():
+	return render_template('lisamurkowski.html') 
+
+@app.route('/massachusetts')
+def massachusetts():
+	return render_template('massachusetts.html') 
+
+@app.route('/matthewlarsen')
+def matthewlarsen():
+	return render_template('matthewlarsen.html') 
+
+@app.route('/matthewstrasburg')
+def matthewstrasburg():
+	return render_template('matthewstrasburg.html')
+	
+@app.route('/mayor')
+def mayor():
+	return render_template('mayor.html') 
+
+@app.route('/newyork')
+def newyork():
+	return render_template('newyork.html') 
+
+@app.route('/paulryan')
+def paulryan():
+	return render_template('paulryan.html') 
+
+@app.route('/president')
+def president():
+	return render_template('president.html')
+	
+@app.route('/representative')
+def representative():
+	return render_template('representative.html') 
+
+@app.route('/richardseifert')
+def richardseifert():
+	return render_template('richardseifert.html') 
+
+@app.route('/senator')
+def senator():
+	return render_template('senator.html') 
+
+@app.route('/steveadler')
+def steveadler():
+	return render_template('steveadler.html')
+	
+@app.route('/tedcruz')
+def tedcruz():
+	return render_template('tedcruz.html') 
+
+@app.route('/texas')
+def texas():
+	return render_template('texas.html') 
+
+@app.route('/wisconsin')
+def wisconsin():
+	return render_template('wisconsin.html') 
 	
 if __name__ == '__main__':
 	app.run(‘162.243.41.174’, ‘80’) # Run application
